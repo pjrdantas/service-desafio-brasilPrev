c5b1701831cc3774eaa6b43b376efd1154ef9116
pjrdantas <pjr_dantas@hotmail.com>
2021-03-09 15:26:57 -0300

    Adicionando a estrutura basica do springboot rest

---
+++ src/main/java/br/com/brasilPrev/ServiceDesadioBrasilPrevApplication.java
@@ -0,0 +1,18 @@
+package br.com.brasilPrev;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDesadioBrasilPrevApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ServiceDesadioBrasilPrevApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDesadioBrasilPrevApplication.class, args);
+		log.info("--------------------------------------------------------------------> SERVIÇO PRONTO PARA SER CONSUMIDO!");
+	}
+
+}
---
+++ src/test/java/servicedesadiobrasilPrev/ServiceDesadioBrasilPrevApplicationTests.java
@@ -0,0 +1,13 @@
+package servicedesadiobrasilPrev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDesadioBrasilPrevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
